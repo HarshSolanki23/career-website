9421a780e1a1c221f70078793f558ea72dc2df1c
HarshSolanki23 <22649382-HarshSolanki23@users.noreply.replit.com>
2023-04-28 12:59:34 +0000

    added HTML and css

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
   return "<h1>Hello world!<h1>"
+
+
+@app.route("/careers/")
+def careers():
+  return render_template("home.html")
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
